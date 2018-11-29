3aa0476c01be23f54d46f70166cfeb21842f8b7a
FRQDO <richard.niland@udo.edu>
2018-11-29 17:00:33 +0100

    REEEEconvert BACK from string to int to actually open the chat this time

--- bot_telegram.py
+++ bot_telegram.py
@@ -205,7 +205,7 @@ def restart_handler(bot, update):
         if len([game for game in existing_games if existing_games["{}".format(game)].game_state!=secret_hitler.GameStates.GAME_OVER])>0 and update.message.text.find('confirm')==-1:
             bot.send_message(chat_id=chat_id, text="{} running game(s) found. Type `/restart confirm` to cancel those games and restart anyway.".format(len(existing_games)))
         else:
-            for game_chat_id in [game for game in existing_games if existing_games["{}".format(game)].game_state!=secret_hitler.GameStates.GAME_OVER]:
+            for game_chat_id in [int(game) for game in existing_games if existing_games["{}".format(game)].game_state!=secret_hitler.GameStates.GAME_OVER]:
                 existing_games["{}".format(game_chat_id)].set_game_state(secret_hitler.GameStates.GAME_OVER)
                 bot.send_message(chat_id=game_chat_id, text="This game has been cancelled. Don’t be sad! Bugfixes and cool new features are coming!")
             # No need to clear the existing_games dict as the bot is shutting down anyway
