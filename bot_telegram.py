239815d6a973ffd09ac87ec0442a8fea49157a37
FRQDO <richard.niland@udo.edu>
2018-11-29 16:32:09 +0100

    Added missing .text call

--- bot_telegram.py
+++ bot_telegram.py
@@ -202,7 +202,7 @@ def restart_handler(bot, update):
     logging.debug("Restart issued by: user_id: %s in chat_id: %s, group admins: %s", user_id, chat_id, admin_ids)
 
     if chat_id == DEV_CHAT_ID and user_id in admin_ids:
-        if len([game for game in existing_games if existing_games[game].game_state!=secret_hitler.GameStates.GAME_OVER])>0 and update.message.find('confirm')==-1:
+        if len([game for game in existing_games if existing_games[game].game_state!=secret_hitler.GameStates.GAME_OVER])>0 and update.message.text.find('confirm')==-1:
             bot.send_message(chat_id=chat_id, text="{} running game(s) found. Type `/restart confirm` to cancel those games and restart anyway.".format(len(existing_games)))
         for game_chat_id in [game for game in existing_games if existing_games[game].game.game_state!=secret_hitler.GameStates.GAME_OVER]:
             existing_games[game_chat_id].set_game_state(secret_hitler.GameStates.GAME_OVER)
