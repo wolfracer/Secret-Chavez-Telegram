043d5796093b5219950d76ef182217ad8f763bd2
FRQDO <richard.niland@udo.edu>
2018-12-18 00:03:15 +0100

    Printing timelog structure via helper method

--- bot_telegram.py
+++ bot_telegram.py
@@ -325,7 +325,7 @@ def game_command_executor(bot, command, args, from_user, chat_id, chat_data, use
     try:
         reply = game.handle_message(chat_id, player, command, args)
         if command == "timelogs":
-            bot.send_message(chat_id=DEV_CHAT_ID, text=game.time_logs)  # DEBUG Print time logs data structure to dev chat
+            bot.send_message(chat_id=DEV_CHAT_ID, text=game.print_time_logs())  # DEBUG Print time logs data structure to dev chat
         # pass all supressed errors (if any) directly to the handler in
         # the order that they occurred
         while len(secret_hitler.telegram_errors) > 0:
