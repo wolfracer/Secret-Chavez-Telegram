b35cb15d9322979b099626817b38245fea64c920
Nicolas Lenz <nicolas@eisfunke.com>
2020-03-29 16:49:16 +0200

    DB test file

---
+++ db_test.py
@@ -0,0 +1,17 @@
+from model import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+test_game = Game(chat=123)
+session.add(test_game)
+test_player = Player(name="Richard", game=test_game)
+session.add(test_player)
+
+#print(session.query(Player).filter_by(name="Richard").first())
+
+session.commit()
